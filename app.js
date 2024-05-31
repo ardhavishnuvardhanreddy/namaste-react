18afcbdd0b93d195ab88a91594eac98f979d0fe5
Student ApnaCollege <vishnuvardhan9473@gmail.com>
2024-06-01 01:30:53 +0530

    cahnged

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("h1",{},"this is namaste react ");
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-
-
-function click(){
-    alert("Click");
-}
